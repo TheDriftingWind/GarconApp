b82d4c2fca5fe4261c082020414b89922dd85d7b
carlyemile <carlyemile@gmail.com>
2017-04-18 15:19:57 -0400

    OwnerFragment branch push

--- app/src/main/java/charleszhu/qunnipiac/edu/gittest/MainActivity.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/gittest/MainActivity.java
@@ -1,7 +1,9 @@
 package charleszhu.qunnipiac.edu.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//hey
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(MainActivity.this, UserActivity.class);
+        if(view.getId()==R.id.owner_button){
+            intent.putExtra("UserType", "Owner"); //Will be used to open the specific fragment
+        }
+
+        else{
+            intent.putExtra("UserType", "Customer");
+        }
+
+        startActivity(intent);
+    }
 }
