f511f69dc0aa6bec9840247e29820d53a9a248ad
carlyemile <carlyemile@gmail.com>
2017-04-18 16:48:30 -0400

    Activities push

--- app/src/main/java/charleszhu/qunnipiac/edu/gittest/OwnerFragment.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/gittest/OwnerFragment.java
@@ -1,11 +1,14 @@
 package charleszhu.qunnipiac.edu.gittest;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.security.acl.Owner;
 
 
 /**
@@ -26,4 +29,19 @@ public class OwnerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_owner, container, false);
     }
 
+
+    public void onClick(View view){
+
+        if(view.getId()==R.id.view_button){
+          //  startActivity(new Intent(getContext(), ViewReservationActivity.class));
+        }
+
+        if(view.getId()==R.id.add_button){
+              startActivity(new Intent(getActivity(), MakeReservationActivity.class));
+        }
+
+        else{
+              startActivity(new Intent(getActivity(), EditReservationActivity.class));
+        }
+    }
 }
---
+++ app/src/main/java/charleszhu/qunnipiac/edu/gittest/ViewReservationActivity.java
@@ -0,0 +1,19 @@
+package charleszhu.qunnipiac.edu.gittest;
+
+import android.database.DataSetObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class ViewReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reservation);
+
+    }
+}
