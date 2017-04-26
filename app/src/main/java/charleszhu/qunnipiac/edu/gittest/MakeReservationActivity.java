e85e5af4db8aa9f52fdde6516d2f4b77cef87608
carlyemile <carlyemile@gmail.com>
2017-04-26 16:33:43 -0400

    make reservation push

--- app/src/main/java/charleszhu/qunnipiac/edu/gittest/MakeReservationActivity.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/gittest/MakeReservationActivity.java
@@ -2,12 +2,22 @@ package charleszhu.qunnipiac.edu.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MakeReservationActivity extends Activity {
 
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private EditText name_et, phone_et, party_et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_reservation);
+
+        datePicker = (DatePicker)findViewById(R.id.datePicker);
+        timePicker = (TimePicker)findViewById(R.id.timePicker2);
+        name_et = (EditText)findViewById(R.id.name_et);
     }
 }
