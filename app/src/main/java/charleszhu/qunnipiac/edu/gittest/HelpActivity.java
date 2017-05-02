9e356acb0d77aa05b91026d76ba13806889df469
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-05-02 17:29:57 -0400

    HelpActivity Reformatted

--- app/src/main/java/charleszhu/qunnipiac/edu/gittest/HelpActivity.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/gittest/HelpActivity.java
@@ -15,16 +15,6 @@ public class HelpActivity extends Activity {
         setContentView(R.layout.activity_help);
 
         TextView description = (TextView)findViewById(R.id.description);
-        description.setText("Make a reservation:\n To make a reservation navigate to " +
-                "the Make Reservation screen and fill out the information requested by the" +
-                "text field \n" +
-                "View a reservation:\n To view a reservation, navigate to the View Reservation" +
-                "screen. Tap on a reservation in the list to view details of that reservation\n" +
-                "Edit a reservation:\n To edit a reservation, navigate to the Edit Reservation" +
-                "screen and tap on the reservation in the list that you'd like to change." +
-                "Enter the new information and press the update button\n" +
-                "Delete a reservation:\n To delete a reservation, navigate to the Edit Reservation" +
-                "screen and tap on the reservation in the list that you'd like to delete." +
-                "Simply press the 'Delete' button to remove. This action cannot be un-done\n");
+
     }
 }
